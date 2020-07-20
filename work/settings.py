bb1974e3e8f2991147c4c1b9188b6d2486d0967a
shion-boss <sss.tl.ges.sss@gmail.com>
2020-07-20 17:09:26 +0900

    Update settings.py

--- work/settings.py
+++ work/settings.py
@@ -134,12 +134,14 @@ try:
 except ImportError:
     pass
 
-if not DEBUG:
-    SECRET_KEY = os.environ['SECRET_KEY']
-    import django_heroku #追加
-    django_heroku.settings(locals()) #追加
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SECRET_KEY = os.environ['SECRET_KEY']
+    import django_heroku #追加
+    django_heroku.settings(locals()) #追加
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
