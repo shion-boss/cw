c82715f8cf3188370f90bc2288d273c5ddf7e47c
shion-boss <sss.tl.ges.sss@gmail.com>
2020-07-20 16:49:59 +0900

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('portfolio1',views.p1_view,name='p1'),
-    path('portfolio2',views.p2_view,name='p2'),
+    path('portfolio1/',views.p1_view,name='p1'),
+    path('portfolio2/',views.p2_view,name='p2'),
 
 ]
